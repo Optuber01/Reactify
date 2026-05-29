7af662229e0b7a8445f6a163e95d0bc72750fc7d
Optuber01 <github@optuber.net>
2026-05-30 00:26:51 +0300

    chore: update build configs and log lessons [skip-signing]

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import flutter_quick_video_encoder
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterQuickVideoEncoderPlugin.register(with: registry.registrar(forPlugin: "FlutterQuickVideoEncoderPlugin"))
 }
